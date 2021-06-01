3d663a6eb1dd80f380cd2a6b5597d226eae4e587
sanchit kumar <74718508+sanchit-kumar-coder@users.noreply.github.com>
2021-06-01 14:10:29 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,20 @@
+const accessCode1 = "VARIABLE";
+const accessCode3 = "FUNCTION";
+const accessCode2 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("REVBAILA", 200,50)
+	//variable
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+	text("ATADASBA", 750,80)
+	text("Hint:where you stores your data !!", 750,50)
+	text("CUTNIFON.", 800,220)
+	text("Hint:can be used again again !!", 750,200)
+// add code for displaying rest of the names and the hints.
+    
+
+}
